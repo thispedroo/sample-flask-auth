3a0cad4ba6e2d3ccf212e70c79165b36e72b7469
João Pedro <jf.pedro09@gmail.com>
2026-03-21 13:03:45 -0300

    updating authentication with flask-login

--- app.py
+++ app.py
@@ -1,13 +1,39 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from models.user import User
 from database import db
+from flask_login import LoginManager, login_user, current_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
+login_manager = LoginManager()
 db.init_app(app)
+login_manager.init_app(app)
+# View Login
+login_manager.login_view = 'login'
 # Session <- Conexão ativa
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+@app.route('/login', methods=["POST"])
+def login():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+    
+    if username and password:
+        # Login
+        user = User.query.filter_by(username=username).first()
+        
+        if user and user.password == password:
+                login_user(user)
+                print(current_user.is_authenticated)
+                return jsonify({"message": "Autenticação realizada com sucesso!"}), 200
+    
+    return jsonify({"message": "Credenciais inválidas!"}), 400
 
 @app.route("/hello-world", methods=["GET"])
 def hello_world():
