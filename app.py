6159e83cd8e8788b14a520d4bc9404f916adc727
João Pedro <jf.pedro09@gmail.com>
2026-03-21 12:12:14 -0300

    creation of the application and database configuration

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from models.user import User
+from database import db
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "your_secret_key"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+db.init_app(app)
+# Session <- Conexão ativa
+
+@app.route("/hello-world", methods=["GET"])
+def hello_world():
+    return "Hello world!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
